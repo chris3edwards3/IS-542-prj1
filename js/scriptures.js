f78456ec5e89e42401cca1a74da3be2689b4d1f7
chris <chris3edwards3@gmail.com>
2020-01-29 00:38:56 -0700

    On Hash Change: navigateHome, navigate Book, bookChapterValid

--- js/scriptures.js
+++ js/scriptures.js
@@ -10,110 +10,180 @@
     console, XMLHttpRequest
  */
 /*property
-    books, forEach, hash, init, log, maxBookId, minBookId, onHashChanged,
-    onerror, onload, open, parse, push, response, send, status
+    books, forEach, getElementById, hash, id, init, innerHTML, length, log,
+    maxBookId, minBookId, onHashChanged, onerror, onload, open, parse, push,
+    response, send, slice, split, status
 */
 let Scriptures = (function () {
-    'use strict';
+  "use strict";
 
-    /*---------------------------------------------------------------
-    *                     CONSTANTS
-    */
+  /*---------------------------------------------------------------
+  *                     CONSTANTS
+  */
 
-    /*---------------------------------------------------------------
-    *                             PRIVATE VARIABLES
-    */
-    let books;
-    let volumes;
+  /*---------------------------------------------------------------
+  *                             PRIVATE VARIABLES
+  */
+  let books;
+  let volumes;
 
-    /*---------------------------------------------------------------
-    *                             PRIVATE METHOD DECLARATIONS
-    */
-    let ajax;
-    let cacheBooks;
-    let init;
-    let onHashChanged;
+  /*---------------------------------------------------------------
+  *                             PRIVATE METHOD DECLARATIONS
+  */
+  let ajax;
+  let bookChapterValid;
+  let cacheBooks;
+  let init;
+  let navigateBook;
+  let navigateChapter;
+  let navigateHome;
+  let onHashChanged;
 
-    /*---------------------------------------------------------------
-    *                             PRIVATE METHODS
-    */
-    ajax = function (url, successCallback, failureCallback) {
-        // Source: http://youmightnotneedjquery.com/
-        let request = new XMLHttpRequest();
-        request.open('GET', url, true);
+  /*---------------------------------------------------------------
+  *                             PRIVATE METHODS
+  */
+  ajax = function (url, successCallback, failureCallback) {
+    // Source: http://youmightnotneedjquery.com/
+    let request = new XMLHttpRequest();
+    request.open("GET", url, true);
 
-        request.onload = function () {
-            if (request.status >= 200 && request.status < 400) {
-                // Success!
-                let data = JSON.parse(request.response);
+    request.onload = function () {
+      if (request.status >= 200 && request.status < 400) {
+        // Success!
+        let data = JSON.parse(request.response);
 
-                if (typeof successCallback === "function") {
-                    successCallback(data);
-                }
-            } else {
-                // We reached our target server, but it returned an error
-                if (typeof failureCallback === "function") {
-                    failureCallback(request);
-                }
-            }
-        };
-
-        request.onerror = failureCallback;
-        request.send();
-    };
-
-    cacheBooks = function (onInitializedCallback) {
-        volumes.forEach(function (volume) {
-            let volumeBooks = [];
-            let bookId = volume.minBookId;
-
-            while (bookId <= volume.maxBookId) {
-                volumeBooks.push(books[bookId]);
-                bookId += 1;
-            }
-
-            volume.books = volumeBooks;
-        });
-
-        if (typeof onInitializedCallback === "function") {
-            onInitializedCallback();
+        if (typeof successCallback === "function") {
+          successCallback(data);
         }
+      } else {
+        // We reached our target server, but it returned an error
+        if (typeof failureCallback === "function") {
+          failureCallback(request);
+        }
+      }
     };
 
-    init = function (onInitializedCallback) {
-        console.log("Started init...");
-        let booksLoaded = false;
-        let volumesLoaded = false;
+    request.onerror = failureCallback;
+    request.send();
+  };
 
-        ajax("https://scriptures.byu.edu/mapscrip/model/books.php", function (data) {
-            books = data;
-            booksLoaded = true;
+  bookChapterValid = function (bookId, chapter) {
+    let book = books[bookId];
 
-            if (volumesLoaded) {
-                cacheBooks(onInitializedCallback);
-            }
-        });
+    if (book === undefined || chapter < 0 || chapter > book.numChapters) {
+      return false;
+    }
 
-        ajax("https://scriptures.byu.edu/mapscrip/model/volumes.php", function (data) {
-            volumes = data;
-            volumesLoaded = true;
+    if (chapter === 0 && book.numChapters > 0) {
+      return false;
+    }
 
-            if (booksLoaded) {
-                cacheBooks(onInitializedCallback);
-            }
-        });
-    };
+    return true;
+  };
 
-    onHashChanged = function () {
-        console.log("The has is " + location.hash);
-    };
+  cacheBooks = function (onInitializedCallback) {
+    volumes.forEach(function (volume) {
+      let volumeBooks = [];
+      let bookId = volume.minBookId;
 
-    /*---------------------------------------------------------------
-    *                                 PUBLIC API
-    */
+      while (bookId <= volume.maxBookId) {
+        volumeBooks.push(books[bookId]);
+        bookId += 1;
+      }
 
-    return {
-        init,
-        onHashChanged
-    };
+      volume.books = volumeBooks;
+    });
+
+    if (typeof onInitializedCallback === "function") {
+      onInitializedCallback();
+    }
+  };
+
+  init = function (onInitializedCallback) {
+    console.log("Started init...");
+    let booksLoaded = false;
+    let volumesLoaded = false;
+
+    ajax("https://scriptures.byu.edu/mapscrip/model/books.php", function (data) {
+      books = data;
+      booksLoaded = true;
+
+      if (volumesLoaded) {
+        cacheBooks(onInitializedCallback);
+      }
+    });
+
+    ajax("https://scriptures.byu.edu/mapscrip/model/volumes.php", function (data) {
+      volumes = data;
+      volumesLoaded = true;
+
+      if (booksLoaded) {
+        cacheBooks(onInitializedCallback);
+      }
+    });
+  };
+
+  navigateBook = function (bookId) {
+    console.log("navigateBook " + bookId);
+  };
+
+  navigateChapter = function (bookId, chapter) {
+    console.log("navigateChapter " + bookId + ", " + chapter);
+  };
+
+  navigateHome = function (volumeId) {
+    document.getElementById("scriptures").innerHTML =
+      "<div>The Old Testament</div>" +
+      "<div>The New Testament</div>" +
+      "<div>The Book of Mormon</div>" +
+      "<div>Doctrine and Covenants</div>" +
+      "<div>The Pearl of Great Price</div>" + volumeId;
+  };
+
+  onHashChanged = function () {
+    let ids = [];
+
+    if (location.hash !== "" && location.hash.length > 1) {
+      ids = location.hash.slice(1).split(":");
+    }
+
+    if (ids.length <= 0) {
+      navigateHome();
+    } else if (ids.length === 1) {
+      let volumeId = Number(ids[0]);
+
+      if (volumeId < volumes[0].id || volumeId > volumes.slice(-1).id) {
+        navigateHome();
+      } else {
+        navigateHome(volumeId);
+      }
+    } else if (ids.length >= 2) {
+      let bookId = Number(ids[1]);
+
+      if (books[bookId] === undefined) {
+        navigateHome();
+      } else {
+        if (ids.length === 2) {
+          navigateBook(bookId);
+        } else {
+          let chapter = Number(ids[2]);
+
+          if (bookChapterValid(bookId, chapter)) {
+            navigateChapter(bookId, chapter);
+          } else {
+            navigateHome();
+          }
+        }
+      }
+    }
+  };
+
+  /*---------------------------------------------------------------
+  *                                 PUBLIC API
+  */
+
+  return {
+    init,
+    onHashChanged
+  };
 }());
